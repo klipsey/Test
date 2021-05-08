2a2c988573385896b14f9b8c6c96773740753548
klipsey <60266015+klipsey@users.noreply.github.com>
2021-05-08 12:34:03 -0700

    thing

---
+++ example.js
@@ -0,0 +1,22 @@
+var table = prompt("What would you like to multiply by?");             // Unit of table
+var operator = 'multiplication'; // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>';              // Message
+
+if (operator === 'multiplication') {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+} else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+}
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
