65df9cd3c2e04ff4164621d129a5515291724055
klipsey <60266015+klipsey@users.noreply.github.com>
2021-05-31 16:01:51 -0700

    2

---
+++ slideshow.js
@@ -0,0 +1,19 @@
+var imageCount = 1;
+var total = 5;
+
+function picture(x) {
+	var image = document.getElementById('image');
+	imageCount = imageCount + x;
+	if(imageCount > total){imageCount = 1;}
+	if(imageCount < 1){imageCount = total;}	
+	image.src = "img"+ imageCount +".JPG";
+								
+	}
+ 
+setInterval(function pictureA() {  
+	var image = document.getElementById('image');
+	imageCount = imageCount + 1;
+	if(imageCount > total){imageCount = 1;}
+	if(imageCount < 1){imageCount = total;}	
+	image.src = "img"+ imageCount +".JPG";
+	},3000);
