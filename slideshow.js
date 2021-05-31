9fa82e8803c90f0f48a1884b2cb5d3e3d1b2320d
klipsey <60266015+klipsey@users.noreply.github.com>
2021-05-31 16:58:31 -0700

    32

--- slideshow.js
+++ slideshow.js
@@ -1,5 +1,8 @@
 var imageCount = 1;
 var total = 5;
+var descCount = 1;
+
+var captionImages =["Best Friend","Paris","Donkey","Astronaut","Pug"];
 
 function picture(x) {
 	var image = document.getElementById('image');
@@ -7,6 +10,7 @@ function picture(x) {
 	if(imageCount > total){imageCount = 1;}
 	if(imageCount < 1){imageCount = total;}	
 	image.src = "img/img"+ imageCount +".jpg";
+
 								
 	}
  
@@ -15,5 +19,21 @@ setInterval(function pictureA() {
 	imageCount = imageCount + 1;
 	if(imageCount > total){imageCount = 1;}
 	if(imageCount < 1){imageCount = total;}	
-	image.src = "img/img"+ imageCount +".jpg";
-	},3000);
+	image.src = "img/img"+ imageCount +".jpg";},3000);
+
+function desc(x) {
+	var caption = document.getElementById('caption');
+	descCount = descCount + x;
+	if(descCount > total){descCount = 1;}
+	if(descCount < 1){descCount = total;}	
+	document.getElementById("caption").innerHTML = captionImages[descCount -1];
+								
+	}
+ 
+setInterval(function descA() {  
+	var caption = document.getElementById('caption');
+	descCount = descCount + 1;
+	if(descCount > total){descCount = 0;}
+	if(descCount < 1){imageCount = total;}	
+	document.getElementById("caption").innerHTML = captionImages[descCount -1];},3000);
+
